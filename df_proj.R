d4cf11d1862e3f3eaafca72909b1bd1a8891048c
Jcoulio <152819091+Jcoulio@users.noreply.github.com>
2024-03-07 16:49:22 -0800

    Add files via upload

--- df_proj.R
+++ df_proj.R
@@ -1,6 +1,9 @@
 library(tidyquant)
 library(tidyverse)
 library(lubridate)
+library(here)
+library(stringr)
+library(purrr)
 
 
 ### Create Dataframes for the portfolio's of 5(or so) best stocks for the different years
@@ -21,7 +24,8 @@ df_stocks_2009 <- stocks_2009_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts4,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2010-01-01") - years(1), .before = 1)
 ## 2010 Portfolio
 stocks_2010_v <- c("CMI", "AKAM", "CRM", "BKNG", "CTBB")
 df_stocks_2010 <- stocks_2010_v %>% 
@@ -34,7 +38,8 @@ df_stocks_2010 <- stocks_2010_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts4,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2011-01-01") - years(1), .before = 1)
 ## 2011 Portfolio
 stocks_2011_v <- c("CTRA", "EP-C", "MA", "HUM", "OKE")
 df_stocks_2011 <- stocks_2011_v %>% 
@@ -47,7 +52,8 @@ df_stocks_2011 <- stocks_2011_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts4,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("20112-01-01") - years(1), .before = 1)
 ## 2012 Portfolio
 stocks_2012_v <- c("SHLD", "BAC", "NFLX", "WHR", "FII")
 df_stocks_2012 <- stocks_2012_v %>% 
@@ -60,7 +66,8 @@ df_stocks_2012 <- stocks_2012_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts3,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2013-01-01") - years(1), .before = 1)
 ## 2013 Portfolio
 stocks_2013_v <- c("NFLX", "BBY", "MU", "DAL", "PBI")
 df_stocks_2013 <- stocks_2013_v %>% 
@@ -73,7 +80,8 @@ df_stocks_2013 <- stocks_2013_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts5,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2014-01-01") - years(1), .before = 1)
 ## 2014 Portfolio
 stocks_2014_v <- c("LUV", "EA", "EW", "AVGO", "AGN")
 df_stocks_2014 <- stocks_2014_v %>% 
@@ -86,7 +94,8 @@ df_stocks_2014 <- stocks_2014_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts4,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2015-01-01") - years(1), .before = 1)
 ## 2015 Portfolio
 stocks_2015_v <- c("NFLX", "AMZN", "ATVI", "NVDA", "HRL")
 df_stocks_2015 <- stocks_2015_v %>% 
@@ -99,7 +108,8 @@ df_stocks_2015 <- stocks_2015_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts4,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2016-01-01") - years(1), .before = 1)
 ## 2016 Portfolio
 stocks_2016_v <- c("NVDA", "OKE", "FCX", "NEM", "SEP")
 df_stocks_2016 <- stocks_2016_v %>% 
@@ -113,7 +123,8 @@ df_stocks_2016 <- stocks_2016_v %>%
                returns_col = Return,
                weights = wts5,
                col_rename = "Return")  %>% 
-  filter(date != "2018-12-17")
+  filter(date != "2018-12-17") %>% 
+  add_row(Return = 0, date = ymd("2017-01-01") - years(1), .before = 1)
 ## 2017 Portfolio
 stocks_2017_v <- c("ALGN", "NRG", "FSLR", "VRTX", "MU")
 df_stocks_2017 <- stocks_2017_v %>% 
@@ -126,7 +137,8 @@ df_stocks_2017 <- stocks_2017_v %>%
   tq_portfolio(assets_col = symbol,
                returns_col = Return,
                weights = wts5,
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2018-01-01") - years(1), .before = 1)
 ## 2018 Portfolio
 stocks_2018_v <- c("AMD", "ABMD", "FTNT", "TRIP", "RHT")
 df_stocks_2018 <- stocks_2018_v %>% 
@@ -140,20 +152,22 @@ df_stocks_2018 <- stocks_2018_v %>%
                returns_col = Return,
                weights = wts4,
                col_rename = "Return") %>% 
-  filter(date != "2022-12-23")
+  filter(date != "2022-12-23") %>% 
+  add_row(Return = 0, date = ymd("2019-01-01") - years(1), .before = 1)
 
 
 ### Create the S&P 500 dataframes for each year
 
 ##SP500 2009
-df_SP500_2009 <- tq_get(x = "SPY",
+df_SP500_2009  %>%  tq_get(x = "SPY",
                         get = "stock.prices",
                         from = "2010-01-01",
                         to = "2014-12-31") %>%
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2010-01-01") - years(1), .before = 1)
 ##SP500 2010
 df_SP500_2010 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -162,7 +176,8 @@ df_SP500_2010 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2011-01-01") - years(1), .before = 1)
 ##SP500 2011
 df_SP500_2011 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -171,7 +186,8 @@ df_SP500_2011 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2012-01-01") - years(1), .before = 1)
 ##SP500 2012
 df_SP500_2012 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -180,7 +196,8 @@ df_SP500_2012 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2013-01-01") - years(1), .before = 1)
 ##SP500 2013
 df_SP500_2013 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -189,7 +206,8 @@ df_SP500_2013 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2014-01-01") - years(1), .before = 1)
 ##SP500 2014
 df_SP500_2014 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -198,7 +216,8 @@ df_SP500_2014 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2015-01-01") - years(1), .before = 1)
 ##SP500 2015
 df_SP500_2015 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -207,7 +226,8 @@ df_SP500_2015 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2016-01-01") - years(1), .before = 1)
 ##SP500 2016
 df_SP500_2016 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -216,7 +236,8 @@ df_SP500_2016 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2017-01-01") - years(1), .before = 1)
 ##SP500 2017
 df_SP500_2017 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -225,7 +246,8 @@ df_SP500_2017 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2018-01-01") - years(1), .before = 1)
 ##SP500 2018
 df_SP500_2018 <- tq_get(x = "SPY",
                         get = "stock.prices",
@@ -234,21 +256,24 @@ df_SP500_2018 <- tq_get(x = "SPY",
   tq_transmute(select = adjusted,
                mutate_fun = periodReturn,
                period = "yearly",
-               col_rename = "Return")
+               col_rename = "Return") %>% 
+  add_row(Return = 0, date = ymd("2019-01-01") - years(1), .before = 1)
 
 
 ### Join each year into its own distinct dataframe with a S&P 500 column and a portfolio column
 
 
+
+
 ##2009
-df_2009_final <- df_SP500_2009 %>% 
+df_2009_final <- df_SP500_2009 %>%
   rename("SP500_Return" = "Return") %>% 
   mutate(portfolio = df_stocks_2009$Return,
          date = ymd(date),
          year_inception = '2009') %>% 
   group_by(year_inception) %>% 
   mutate(SP500_Cum = cumprod(1 + SP500_Return),
-         RAND_Cum = cumprod(1 + portfolio))
+         RAND_Cum = cumprod(1 + portfolio))f
 ##2010
 df_2010_final <- df_SP500_2010 %>% 
   rename("SP500_Return" = "Return") %>% 
@@ -330,3 +355,8 @@ df_2018_final <- df_SP500_2018 %>%
   group_by(year_inception) %>% 
   mutate(SP500_Cum = cumprod(1 + SP500_Return),
          RAND_Cum = cumprod(1 + portfolio))
+
+
+
+  
+  
