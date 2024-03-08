6c0830f47221f7a00c21d8d03d214c20d1474532
Jcoulio <152819091+Jcoulio@users.noreply.github.com>
2024-03-08 14:01:01 -0800

    Add files via upload

--- app.R
+++ app.R
@@ -13,6 +13,7 @@ library(lubridate)
 library(tidyverse)
 library(DT)
 df_total_final <- readRDS("final_data_331.rds")
+df_names_final <- readRDS("final_names_331.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -27,20 +28,23 @@ ui <- fluidPage(
                   "Select Year:",
                   min = 2009,
                   max = 2018,
-                  value = 2014),
+                  value = 2014,
+                  sep = ""),
       sliderInput("investmentAmount",
                   "Select Investment Amount:",
                   min = 1000,
                   max = 5000,
                   value = 3000, # Default value
-                  step = 250)
+                  step = 250),
+      
+      DTOutput("StocksTable")
       
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("Plot", width = "100%", height = "500px"), # Adjust width and height as needed
-      DTOutput("investmentTable") # Add this line for the table output
+      plotOutput("Plot", width = "100%", height = "500px"),
+      DTOutput("investmentTable")
     )
   )
 )
@@ -58,11 +62,11 @@ server <- function(input, output) {
     df %>%
       ggplot(aes(x = date)) +
       geom_line(aes(y = SP500_Cum * input$investmentAmount, color = "S&P 500"), linetype = "solid") +
-      geom_line(aes(y = RAND_Cum * input$investmentAmount, color = "Created Portfolio"), linetype = "solid") +
+      geom_line(aes(y = RAND_Cum * input$investmentAmount, color = "Top Preforming Stocks"), linetype = "solid") +
       labs(title = "Portfolio Growth Comparison",
            subtitle = "Investment Value",
            y = NULL,
-           x = "Years After Start Date",
+           x = "Next Five Years After Investing",
            color = "Legend") +
       scale_y_continuous(limits = c(0, 35000), breaks = scales::pretty_breaks(n = 5)) +
       theme_bw()
@@ -77,16 +81,34 @@ server <- function(input, output) {
     lastRow <- tail(df, 1)
     
     dataTable <- data.frame(
-      "Investment" = c("S&P 500", "Top Performing Stocks"),
-      "Final Investment Value" = c(round(lastRow$SP500_Cum[1] * input$investmentAmount, 2), round(lastRow$RAND_Cum[1] * input$investmentAmount, 2))
-    )
+      "Investment" = c("S&P 500", "Portfolio of Top Performing Stocks This Year"),
+      "Final Investment Value After Five Years" = c(round(lastRow$SP500_Cum[1] * input$investmentAmount, 2), round(lastRow$RAND_Cum[1] * input$investmentAmount, 2)),
+    check.names = FALSE)
     
-    datatable(dataTable, options = list(searching = FALSE, lengthChange = FALSE, paging = FALSE, info = FALSE))
+    datatable(dataTable, options = list(searching = FALSE, lengthChange = FALSE, paging = FALSE, info = FALSE),
+              rownames = FALSE)
     
     
   })
   
-}
+
+  output$StocksTable <- renderDT({
+    
+    df_names <- df_names_final %>% 
+      filter(year == as.character(input$Year))
+    
+    # Convert the vector of characters to a single string
+    
+    dataTable <- data.frame(
+    "Ticker" = c(df_names$ticker),
+    "Company Name" = c(df_names$name)
+    )
+    datatable(dataTable, options = list(searching = FALSE, lengthChange = FALSE, paging = FALSE, info = FALSE),
+              rownames = FALSE, # Remove row names if not needed
+              caption = htmltools::tags$caption(htmltools::tags$h3("Best Performing Stocks This Year")))
+})
+  
+}  
 
 
 # Run the application 
